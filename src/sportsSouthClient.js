6276d560574fb74be52d8d6902e684288e116722
Decodingcoder <134673860+Decodingcoder@users.noreply.github.com>
2025-06-17 19:42:17 +0100

    sportsSouthClient.js

--- src/sportsSouthClient.js
+++ src/sportsSouthClient.js
@@ -5,23 +5,23 @@ const xml2js = require('xml2js');
 
 const WS_BASE = 'http://webservices.theshootingwarehouse.com/smart';
 const {
-  SS_USERNAME,
-  SS_PASSWORD,
-  SS_CUSTOMER_NUMBER,
-  SS_SOURCE
+  SPORTS_SOUTH_USERNAME,
+  SPORTS_SOUTH_PASSWORD,
+  SPORTS_SOUTH_CUSTOMER_NUMBER,
+  SPORTS_SOUTH_SOURCE
 } = process.env;
 
 async function fetchSportsSouthInventory(sinceIso = '1990-01-01T00:00:00Z') {
-  if (!SS_USERNAME || !SS_PASSWORD || !SS_CUSTOMER_NUMBER || !SS_SOURCE) {
+  if (!SPORTS_SOUTH_USERNAME || !SPORTS_SOUTH_PASSWORD || !SPORTS_SOUTH_CUSTOMER_NUMBER || !SPORTS_SOUTH_SOURCE) {
     throw new Error('Missing one or more required Sports South credentials in environment variables.');
   }
 
   const resp = await axios.get(`${WS_BASE}/inventory.asmx/IncrementalOnhandUpdate`, {
     params: {
-      UserName:       SS_USERNAME,
-      Password:       SS_PASSWORD,
-      CustomerNumber: SS_CUSTOMER_NUMBER,
-      Source:         SS_SOURCE,
+      UserName:       SPORTS_SOUTH_USERNAME,
+      Password:       SPORTS_SOUTH_PASSWORD,
+      CustomerNumber: SPORTS_SOUTH_CUSTOMER_NUMBER,
+      Source:         SPORTS_SOUTH_SOURCE,
       SinceDateTime:  sinceIso
     },
     headers: { 'Accept': 'text/xml' }
